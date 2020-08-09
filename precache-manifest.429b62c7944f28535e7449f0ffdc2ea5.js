9ff88f65ed3c299dc3ba48530e0f43142eb5880c
Arun Chaudhary <arunchaudharyweb@gmail.com>
2020-08-09 13:23:26 +0530

    Updates

---
+++ precache-manifest.429b62c7944f28535e7449f0ffdc2ea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16cf67b45de9ae3e7b5b4ae421b0f0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c22d6c756c0a1a98715",
+    "url": "/static/js/2.35e80a63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.35e80a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c02d1d848461445a3746",
+    "url": "/static/js/main.249456c1.chunk.js"
+  },
+  {
+    "revision": "37b2154ae4a4db6f3932",
+    "url": "/static/js/runtime-main.9490d2a5.js"
+  }
+]);
