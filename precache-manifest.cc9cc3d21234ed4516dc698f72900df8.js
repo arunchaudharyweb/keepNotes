d87766ec9463b8edecb02902c65abd56c3db3c8f
Arun Chaudhary <arunchaudharyweb@gmail.com>
2020-08-09 13:43:14 +0530

    Updates

---
+++ precache-manifest.cc9cc3d21234ed4516dc698f72900df8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972e287cb9fab337f91dfde1fb6fdb2a",
+    "url": "/keepNotes/index.html"
+  },
+  {
+    "revision": "9c22d6c756c0a1a98715",
+    "url": "/keepNotes/static/js/2.35e80a63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keepNotes/static/js/2.35e80a63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdf2dcc4b7f4d5368eb0",
+    "url": "/keepNotes/static/js/main.c49ed437.chunk.js"
+  },
+  {
+    "revision": "0b7054962525dcaf51aa",
+    "url": "/keepNotes/static/js/runtime-main.239e6e3f.js"
+  }
+]);
